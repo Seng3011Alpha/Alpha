f57cb840305319128cc1f94421c6dff595331590
ChienKai <z5505220@ad.unsw.edu.au>
2026-04-03 13:31:16 +1100

    Delete testnewsapi.py

--- testnewsapi.py
+++
@@ -1,9 +0,0 @@
-from newsapi import NewsApiClient
-
-# Init
-newsapi = NewsApiClient(api_key='48806b5843074639b77860448da1687a')
-
-# /v2/everything
-all_articles = newsapi.get_top_headlines(country='au',
-                                      language='en',)
-print(all_articles)
