6b65888541208597de5dbd9da9ffc5378c054c2e
ChienKai <z5505220@ad.unsw.edu.au>
2026-04-03 13:31:23 +1100

    Delete testyhfinance.py

--- testyhfinance.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-dat = yf.Ticker("MSFT")
-dat.info
-dat.calendar
-dat.analyst_price_targets
-dat.quarterly_income_stmt
-dat.history(period='1mo')
-dat.option_chain(dat.options[0]).calls
